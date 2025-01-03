d50ba3657f5eb6de8c78c5479cb2db6038561ad9
Your Name <your.email@example.com>
2025-01-03 15:15:15 +0630

    Update CORS configuration to allow all origins

--- myanmar-betting/backend/config/cors.php
+++ myanmar-betting/backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://myanmar-betting.onrender.com', 'http://localhost:62860'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
